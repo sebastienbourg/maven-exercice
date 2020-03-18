7395c67b739d2dfdf0ef79982fb8bbc0d7fae0cd
sebastienbourg <sebastien.bourg@dauphine.eu>
2020-03-18 09:35:54 +0100

    second one

--- src/main/java/io/github/sebastienbourg/mavenexercice/maven_exercice/DiceUser.java
+++ src/main/java/io/github/sebastienbourg/mavenexercice/maven_exercice/DiceUser.java
@@ -4,17 +4,21 @@ public class DiceUser {
 
 	public static TripletOfDice rollOnce() {
 		TripletOfDice dice;
-		dice = new TripletOfDice();
-		dice.rollAllDie();
+		dice = new TripletOfDice(6);
+		dice.rollAllDie(6);
 		
 		return dice;
 	}
 	
 	
 	public static void main(String[] args) {
-		de = rollOnce();
+		TripletOfDice dice;
+		dice = rollOnce();
+		
 		
 		System.out.println("The die number 1 is a ", dice.getFirstDie());
+		System.out.println("The die number 2 is a ", dice.getFirstDie());
+		System.out.println("The die number 3 is a ", dice.getFirstDie());
 
 	}
 
--- src/main/java/io/github/sebastienbourg/mavenexercice/maven_exercice/TripletOfDice.java
+++ src/main/java/io/github/sebastienbourg/mavenexercice/maven_exercice/TripletOfDice.java
@@ -12,33 +12,33 @@ public class TripletOfDice {
 	/**
 	 * @param args
 	 */
-	
+	   private int number_face;
 	   private int die1;   // Number showing on the first die.
 	   private int die2;   // Number showing on the second die.
 	   private int die3;   // Number showing on the second die.
 	   
-	   public TripletOfDice() {
-		   rollAllDie();  // Call the roll() method to roll the dice.
+	   public TripletOfDice(int number_face) {
+		   rollAllDie(number_face);  // Call the roll() method to roll the dice.
 	}
 	
 	/**
 	 * Roll the dice by setting each die to be a random number between 1 and 6.
 	 */
-	public void rollAllDie() {
-	    die1 = (int)(Math.random()*6) + 1;
-	    die2 = (int)(Math.random()*6) + 1;
-	    die3 = (int)(Math.random()*6) + 1;
+	public void rollAllDie(int number_face) {
+	    die1 = (int)(Math.random()*number_face) + 1;
+	    die2 = (int)(Math.random()*number_face) + 1;
+	    die3 = (int)(Math.random()*number_face) + 1;
 	}
 	
-	public void rollOneDie(int die_number) {
+	public void rollOneDie(int die_number, int number_face) {
 		if (die_number == 1) {
-			die1 = (int)(Math.random()*6) + 1;
+			die1 = (int)(Math.random()*number_face) + 1;
 		}
 		else if (die_number == 2) {
-			die2 = (int)(Math.random()*6) + 1;
+			die2 = (int)(Math.random()*number_face) + 1;
 		}
 		else if (die_number == 3) {
-			die3 = (int)(Math.random()*6) + 1;
+			die3 = (int)(Math.random()*number_face) + 1;
 		}
 		else {
 			IllegalArgumentException exc = new IllegalArgumentException("Value must be in 1,2,3.");
@@ -46,14 +46,14 @@ public class TripletOfDice {
 		}
 	}
 	    
-	public int getFirstDie() {
-		return die1;
+	public String getFirstDie() {
+		return String.valueOf(die1);
 	    }
-	public int getSecondDie() {
-	       return die2;
+	public String getSecondDie() {
+	       return String.valueOf(die2);
 	    }
-	public int getThridDie() {
-	       return die3;
+	public String getThridDie() {
+	       return String.valueOf(die3);
 	    }
 	
 	public void setFirstDie( int value ) {
